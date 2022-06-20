6ad4a248ecffe3d8cd9c9688398ba97897c37104
ndivhuho22 <mphelonn@gmail.com>
2022-06-20 16:33:06 +0200

    portfolio commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const skillsList = [
+    { title: "React JS", value: 75 },
+    { title: "c++", value: 80 },
+    { title: "c#", value: 80 },
+    { title: "css3", value: 80 },
+    { title: "html5 ", value: 60 },
+    { title: "Bootstrap ", value: 70 },
+    { title: "Flutter", value: 70 },
+    { title: "SQL", value: 70 },
+    { title: "PL/SQL", value: 70 },
+    { title: "Javascript", value: 70 },
+];
+
+
+const experienceList = [
+    {
+        id: 0,
+        company: "Gijima",
+        links: {
+            website: "https://www.gijima.com/",
+            
+        },
+    },
+    {
+        id: 1,
+        company: "Limpopo Department of Agriculture and Rural Development",
+        links: {
+            website: "https://www.ldard.gov.za/",
+        },
+    },
+];
+
+export { skillsList,  experienceList };
